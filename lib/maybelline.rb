82c815bb3d25c6225d8b1380003181b2e17ffcd9
Allen Madsen <allen.c.madsen@gmail.com>
2012-06-24 10:23:42 -0400

    Add delegation for more methods.

--- lib/maybelline.rb
+++ lib/maybelline.rb
@@ -22,8 +22,36 @@ module Maybelline
       ::Object.new.send :throw, :nothing if @__object__.nil?      
     end
 
+    def ==(other)
+      @__object__ == other
+    end
+
+    def equal?(other)
+      @__object__.equal?(other)
+    end
+
+    def instance_eval(*args)
+      if block_given?
+        @__object__.instance_eval(*args, &Proc.new)
+      else
+        @__object__.instance_eval(*args)
+      end
+    end
+
+    def instance_exec(*args)
+      if block_given?
+        @__object__.instance_exec(*args, &Proc.new)
+      else
+        @__object__.instance_exec(*args)
+      end
+    end
+
     def method_missing(method, *args)
-      result = @__object__.send(method, *args)
+      result = if block_given?
+                 @__object__.__send__(method, *args, &Proc.new)
+               else
+                 @__object__.__send__(method, *args)
+               end
 
       ::Object.new.send :throw, :nothing if result.nil?
 
